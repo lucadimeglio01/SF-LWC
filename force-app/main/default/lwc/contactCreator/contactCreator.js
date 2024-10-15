57a991f44b8289aa34020d1b2c88cbc5af4cd9fa
provaGit <lucadime11@gmail.com>
2024-10-15 09:35:39 +0200

    contactCreator

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -1,25 +1,3 @@
-/*
-Use lightning-record-form to create a Lightning web component that allows you to create contact records.
-
-Tip: Remember that JavaScript is a case-sensitive programming language. Be careful to use uppercase and lowercase letters correctly.
-
-Create a Lightning web component:
-Name: contactCreator
-Base component: lightning-record-form
-- Fields to include: FirstName, LastName, Email
-
-Implement an event handler:
-Name: handleSuccess
-Event: onsuccess
-Action: Show the Id of the created contact in a toast message
-Add the component to the Working with Data page
-
-Use the contactCreator component to create a contact:
-First name: Lisa
-Last name: Jones
-Email: ljones@developer.com
-*/
-
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
