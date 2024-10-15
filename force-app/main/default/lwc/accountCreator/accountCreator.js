d603b8d906dcebd4362800ec3f3d5edb28fb59e9
Luca Di Meglio <luca.dimeglio001@studenti.uniparthenope.it>
2024-10-15 11:17:54 +0200

    accountCreator

---
+++ force-app/main/default/lwc/accountCreator/accountCreator.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import NAMEACC_FIELD from '@salesforce/schema/Account.Name';
+import NUMBERACC_FIELD from '@salesforce/schema/Account.AccountNumber';
+
+export default class AccountCreator extends LightningElement {
+
+    objectApiName = ACCOUNT_OBJECT;
+    fields = [NAMEACC_FIELD, NUMBERACC_FIELD];
+
+    handleSuccess(event){
+        const evt = new ShowToastEvent({
+            title: 'Account Created!',
+            message: 'Name Account: ' + NAMEACC_FIELD + ', ID: ' + event.detail.id
+        });
+        this.dispatchEvent(evt);
+    }
+}
