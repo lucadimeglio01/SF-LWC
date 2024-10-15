3250c9241ad785395cb609d56aa6e102d77dbcf7
Luca Di Meglio <luca.dimeglio001@studenti.uniparthenope.it>
2024-10-15 12:58:40 +0200

    commit

--- force-app/main/default/lwc/accountCreator/accountCreator.js
+++ force-app/main/default/lwc/accountCreator/accountCreator.js
@@ -1,5 +1,4 @@
-import { LightningElement, api, wire} from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAMEACC_FIELD from '@salesforce/schema/Account.Name';
@@ -9,10 +8,6 @@ export default class AccountCreator extends LightningElement {
 
     objectApiName = ACCOUNT_OBJECT;
     fields = [NAMEACC_FIELD, NUMBERACC_FIELD];
-
-    @api recordId;
-    @wire(getRecord, { recordId: "$recordId", fields: [NAMEACC_FIELD] })
-    account;
 
     handleSuccess(event){
         const evt = new ShowToastEvent({
