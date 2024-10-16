67a287a77bc4d1937d50fb747a6b2fe1008ced8e
Luca Di Meglio <luca.dimeglio001@studenti.uniparthenope.it>
2024-10-16 09:33:30 +0200

    contactController

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,19 @@
+import { LightningElement, wire } from 'lwc';
+import FirstName_FIELD from '@salesforce/schema/Contact.FirstName';
+import LastName_FIELD from '@salesforce/schema/Contact.LastName';
+import Email_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+const COLUMNS = [
+    { label: 'Account FirstName', fieldName: FirstName_FIELD.fieldApiName, type: 'text' },
+    { label: 'Account LastName', fieldName: LastName_FIELD.fieldApiName, type: 'text' },
+    { label: 'Account Email', fieldName: Email_FIELD.fieldApiName, type: 'text' }
+];
+
+export default class ContactList extends LightningElement {
+
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+
+}
